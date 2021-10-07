a138be0b186acd764e901bcd83790dec46be0500
dsitd <dsitd@yandex.com>
2021-10-07 15:52:37 +0300

    вторая задачка, очень забавная

--- 08.10 olympic/08.10 olympic.cpp
+++ 08.10 olympic/08.10 olympic.cpp
@@ -1,7 +1,16 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
+using namespace std;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int n = 0;
+	cin >> n;
+	int a = 0;
+	for (int i = 0; i < n; ++i) {
+		cin >> a;
+		cout << a << endl;
+	}
+	/// так как что делать если число простое, не сказано - значит простых нет
 }
+
