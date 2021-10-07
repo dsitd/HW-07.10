65774650607b6c7055833f04a8f90306848fbef0
dsitd <dsitd@yandex.com>
2021-10-07 17:41:58 +0300

    Костыли моя жизнь!

--- 08.10 olympic/08.10 olympic.cpp
+++ 08.10 olympic/08.10 olympic.cpp
@@ -5,5 +5,21 @@ using namespace std;
 int main()
 {
 	int n = 0;
+	int m = 0;
+	int f = 0;
+	int t = 0;
 	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		cin >> m >> f;
+		for (int j = 0; j < 2 * m ; ++j) {
+			if (j * j % m == f) {
+				cout << "Yes" << endl;
+				t = 1; 
+				break;
+			}
+		}
+		if (t == 0) {
+			cout << "No" << endl;
+		}
+	}
 }
