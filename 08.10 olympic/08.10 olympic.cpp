f4882bd08f388a2ddf9bff9b61a04679f17d63de
dsitd <dsitd@yandex.com>
2021-10-07 23:18:34 +0300

    Бомбезная формула

--- 08.10 olympic/08.10 olympic.cpp
+++ 08.10 olympic/08.10 olympic.cpp
@@ -3,7 +3,23 @@ using namespace std;
 
 
 int main()
-{
-	int n = 0;
-	cin >> n;
+{	
+	int v1; int v2; int v3;
+	cin >> v1 >> v2 >> v3;
+	int a = max(v1, v2);
+	int b = min(v2, v1);
+	if ((v3 == a) || (v3 == b) || (a - (b - (a % b)))) {
+		cout << "Yes" << endl;
+	}
+	else {
+		while (a > 0) {
+			if (a == v3) {
+				cout << "Yes" << endl;
+				break;
+			}
+			a -= b;
+		}
+		cout << "No" << endl;
+	}
+	return 0;
 }
