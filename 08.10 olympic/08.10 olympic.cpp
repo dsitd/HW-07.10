e973c82161d5e2b069ed79af0c1eb1043f1b824f
dsitd <dsitd@yandex.com>
2021-10-28 23:56:09 +0300

    good

--- 08.10 olympic/08.10 olympic.cpp
+++ 08.10 olympic/08.10 olympic.cpp
@@ -2,8 +2,35 @@
 using namespace std;
 
 
-int main()
-{
-	int n = 0;
-	cin >> n;
+
+int fib_rec(int n) {
+	if (n <= 1) {
+		return 1;
+	}
+	else {
+		return fib_rec(n - 1) + fib_rec(n - 2);
+	}
+}
+
+int fib_for(int n) {
+	int a = 0;
+	int b = 1;
+	int c;
+	for (int i = 1; i <= n; ++i) {
+		c = b;
+		b += a;
+		a = c;
+	}
+	return b;
+}
+
+int Kill_bill(int n = 0) {
+	return Kill_bill(n) * 1;
+}
+
+
+
+int main() {
+	Kill_bill();
+	return 0;
 }
